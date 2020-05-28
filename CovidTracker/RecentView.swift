82aa48a9cfae0e011031b6580c65a297cba22e6f
Yanbing Fang <yanbingfang@utexas.edu>
2020-05-27 19:07:21 -0500

    Add search and country detail view

--- CovidTracker/RecentView.swift
+++ CovidTracker/RecentView.swift
@@ -13,10 +13,15 @@ struct RecentView: View {
     
     @ObservedObject var covidFetch = CovidFetchRequest()
     @State var searchText = ""
+    @State var isSearchVisible = false
     
     var body: some View {
         NavigationView{
             VStack{
+                
+                if isSearchVisible{
+                    SearchView(searchText: $searchText)
+                }
                 TotalDataView(totalData:covidFetch.totalData)
                 ListHeaderView()
                 List{
@@ -24,12 +29,29 @@ struct RecentView: View {
                     ForEach(covidFetch.allCountries.filter{
                         self.searchText.isEmpty ? true : $0.country.lowercased().contains(self.searchText.lowercased())
                     }, id:\.country){countryData in
-                        CountryDataRowView(countryData: countryData)
-
+                        
+                        NavigationLink(destination:
+                            CountryDetailView(countryData:countryData)){
+                            CountryDataRowView(countryData: countryData)
+                        }
                     }
                 }
                 
             }//End of VStack
+                .navigationBarTitle("Recent Data",displayMode: .inline)
+                .navigationBarItems(trailing:
+                    Button(action: {
+                        
+                        self.isSearchVisible.toggle()
+                        
+                        if !self.isSearchVisible {
+                            self.searchText = ""
+                        }
+                        
+                        },label:{
+                            Image(systemName: "magnifyingglass")
+                    })
+        )
         }//End of navigation view
     }
 }
